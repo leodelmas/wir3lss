5bb277575eb7965f2ba3680e60b0aa04e9901394
Léo Delmas <ldelmaspro@gmail.com>
2022-01-06 13:34:42 +0100

    Log index route + initial template

---
+++ src/Controller/LogController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\LogRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/log')]
+class LogController extends AbstractController
+{
+    #[Route('/', name: 'log_index', methods: ['GET'])]
+    public function index(LogRepository $logRepository): Response
+    {
+        return $this->render('log/index.html.twig', [
+            'logs' => $logRepository->findAll(),
+        ]);
+    }
+}
