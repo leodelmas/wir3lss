02bdca79a15b2c7f818cff5d85785066374bf29a
Léo Delmas <ldelmaspro@gmail.com>
2022-01-06 13:25:53 +0100

    Log entity

---
+++ src/Entity/Log.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LogRepository::class)]
+class Log
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $ip;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $mac;
+
+    #[ORM\Column(type: 'datetime')]
+    private $sented;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $result;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIp(): ?string
+    {
+        return $this->ip;
+    }
+
+    public function setIp(string $ip): self
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    public function getMac(): ?string
+    {
+        return $this->mac;
+    }
+
+    public function setMac(string $mac): self
+    {
+        $this->mac = $mac;
+
+        return $this;
+    }
+
+    public function getSented(): ?\DateTimeInterface
+    {
+        return $this->sented;
+    }
+
+    public function setSented(\DateTimeInterface $sented): self
+    {
+        $this->sented = $sented;
+
+        return $this;
+    }
+
+    public function getResult(): ?string
+    {
+        return $this->result;
+    }
+
+    public function setResult(string $result): self
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+}
