d08b2a6246db4ccfefa59fe5e62aa61bb9b2ecf2
Léo Delmas <ldelmaspro@gmail.com>
2022-02-27 17:49:53 +0100

    Pagination + navbar

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -3,18 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\LogRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/log')]
 class LogController extends AbstractController
 {
     #[Route('/', name: 'log_index', methods: ['GET'])]
-    public function index(LogRepository $logRepository): Response
+    public function index(LogRepository $logRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $logs = $paginator->paginate(
+            $logRepository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('log/index.html.twig', [
-            'logs' => $logRepository->findAll(),
+            'logs' => $logs,
         ]);
     }
 }
