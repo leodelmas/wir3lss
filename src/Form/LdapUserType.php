288bc6cef79b0e00dcdf7eca714049169a1c7e72
leodelmas <ldelmaspro@gmail.com>
2022-10-24 21:09:09 +0200

    🚧 Help for fields

--- src/Form/LdapUserType.php
+++ src/Form/LdapUserType.php
@@ -16,13 +16,20 @@ class LdapUserType extends AbstractType
     {
         $builder
             ->add('cn', TextType::class, [
+                'help' => 'L\'identifiant de l\'utilisateur : 1 chaîne de caractères, en minuscule - Exemple : jdubois',
                 'attr' => [
                     'readonly' => $options['edit']
                 ]
             ])
-            ->add('displayedName', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('phone', TextType::class)
+            ->add('displayedName', TextType::class, [
+                'help' => 'Le nom complet de l\'utilisateur affiché - Exemple : Jean DUBOIS'
+            ])
+            ->add('email', EmailType::class, [
+                'help' => 'L\'adresse e-mail de l\'utilisateur - Exemple : jdubois@gmail.com'
+            ])
+            ->add('phone', TextType::class, [
+                'help' => 'Le numéro de téléphone au format français - Exemple : 0601020304'
+            ])
             ->add('password', PasswordType::class, [
                 'help' => 'Le mot de passe doit contenir au moins : 10 caractères, 1 majuscule, 1 chiffre, 1 caractère spécial'
             ])
